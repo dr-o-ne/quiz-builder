669924a210b48e912338b5f1fda247d5a866df95
Serhii Rudiuk <virimm@gmail.com>
2020-04-18 15:43:07 +0300

    add web api project
    create and initial setup web api

---
+++ quiz-builder-server/quiz-builder-api/Controllers/QuizzesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace quiz_builder_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuizzesController : ControllerBase
+    {
+        private readonly ILogger<QuizzesController> _logger;
+
+        public QuizzesController(ILogger<QuizzesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Quiz> Get()
+        {
+            return new Quiz[]
+            {
+                new Quiz()
+                {
+                    Id = 1,
+                    Name = "First Quiz"
+                },
+                new Quiz()
+                {
+                    Id = 2,
+                    Name = "Second Quiz"
+                },
+                new Quiz()
+                {
+                    Id = 3,
+                    Name = "Third Quiz"
+                },
+            };
+        }
+    }
+}
---
+++ quiz-builder-server/quiz-builder-api/Quiz.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace quiz_builder_api
+{
+    public class Quiz
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
