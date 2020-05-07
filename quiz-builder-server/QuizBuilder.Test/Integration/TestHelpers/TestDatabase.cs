ece8cf818f716160e88ca33b9f1b5d369a2a3832
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-05-07 22:13:29 +0300

    move hacks into TestDb class

--- quiz-builder-server/QuizBuilder.Test/Integration/TestHelpers/TestDatabase.cs
+++ quiz-builder-server/QuizBuilder.Test/Integration/TestHelpers/TestDatabase.cs
@@ -2,10 +2,34 @@
 using System.Data;
 using ServiceStack.OrmLite;
 using ServiceStack.OrmLite.Sqlite;
+using static System.Text.Encoding;
+using static Dapper.SqlMapper;
 
 namespace QuizBuilder.Test.Integration.TestHelpers {
 
 	public sealed class TestDatabase {
+
+		//https://stackoverflow.com/questions/25568657/dapper-typehandler-setvalue-not-being-called
+		public sealed class SqliteOrmLiteGuidTypeHandler : TypeHandler<Guid> {
+
+			public override Guid Parse( object value ) {
+				byte[] inVal = UTF8.GetBytes( value?.ToString() ?? string.Empty );
+				byte[] outVal = { inVal[3], inVal[2], inVal[1], inVal[0], inVal[5], inVal[4], inVal[7], inVal[6], inVal[8], inVal[9], inVal[10], inVal[11], inVal[12], inVal[13], inVal[14], inVal[15] };
+				return new Guid( outVal );
+			}
+
+			public override void SetValue( IDbDataParameter parameter, Guid value ) {
+				var inVal = value.ToByteArray();
+				byte[] outVal = { inVal[3], inVal[2], inVal[1], inVal[0], inVal[5], inVal[4], inVal[7], inVal[6], inVal[8], inVal[9], inVal[10], inVal[11], inVal[12], inVal[13], inVal[14], inVal[15] };
+				parameter.Value = outVal;
+			}
+		}
+
+		static TestDatabase() {
+			AddTypeHandler( new SqliteOrmLiteGuidTypeHandler() );
+			RemoveTypeMap( typeof( Guid ) );
+			RemoveTypeMap( typeof( Guid? ) );
+		}
 
 		public readonly OrmLiteConnectionFactory ConnectionFactory = new OrmLiteConnectionFactory( ":memory:", SqliteOrmLiteDialectProvider.Instance );
 
