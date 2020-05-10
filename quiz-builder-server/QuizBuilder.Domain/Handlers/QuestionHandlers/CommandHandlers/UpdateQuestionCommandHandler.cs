92d580b9a8e614b6d76f0df676c622adc2723cfe
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-05-10 20:46:16 +0300

    fix

--- quiz-builder-server/QuizBuilder.Domain/Handlers/QuestionHandlers/CommandHandlers/UpdateQuestionCommandHandler.cs
+++ quiz-builder-server/QuizBuilder.Domain/Handlers/QuestionHandlers/CommandHandlers/UpdateQuestionCommandHandler.cs
@@ -3,7 +3,6 @@ using AutoMapper;
 using QuizBuilder.Common.Handlers;
 using QuizBuilder.Common.Types.Default;
 using QuizBuilder.Domain.Commands.QuestionCommands;
-using QuizBuilder.Domain.Extensions;
 using QuizBuilder.Domain.Model.Default.Questions;
 using QuizBuilder.Repository.Dto;
 using QuizBuilder.Repository.Repository;
@@ -23,15 +22,16 @@ namespace QuizBuilder.Domain.Handlers.QuestionHandlers.CommandHandlers {
 
 		public async Task<CommandResult> HandleAsync( UpdateQuestionCommand command ) {
 
-			QuestionDto questionDto = await _questionRepository.GetByUIdAsync( command.Id );
+			QuestionDto currentQuestionDto = await _questionRepository.GetByUIdAsync( command.Id );
 
-			Question currentQuestion = _mapper.Map<QuestionDto, Question>( questionDto );
-			Question updatedQuestion = _mapper.Map<UpdateQuestionCommand, Question>( command );
-			Question mergedQuestion = _mapper.Merge( updatedQuestion, currentQuestion );
-			QuestionDto mergedQuestionDto = _mapper.Map<Question, QuestionDto>( mergedQuestion );
-			int rowsAffected = await _questionRepository.UpdateAsync( mergedQuestionDto );
+			Question question = _mapper.Map<UpdateQuestionCommand, Question>( command );
+
+			question.Id = currentQuestionDto.Id;
+			QuestionDto questionDto = _mapper.Map<Question, QuestionDto>( question );
+			int rowsAffected = await _questionRepository.UpdateAsync( questionDto );
 
 			return new CommandResult( success: rowsAffected.GreaterThanZero(), message: string.Empty );
+
 		}
 	}
 }
