379122c18df43e06277d7b50580f3f03c95e8f47
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-05-30 19:57:47 +0300

    material module and guards

---
+++ client/src/app/_material/material.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSortModule } from '@angular/material/sort';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@NgModule({
+  exports: [
+        MatButtonModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatTabsModule,
+        MatIconModule,
+        MatListModule,
+        MatCheckboxModule,
+        MatPaginatorModule,
+        MatTableModule,
+        MatDialogModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        MatSortModule,
+        MatNativeDateModule,
+        MatRippleModule,
+        DragDropModule,
+        MatRadioModule,
+        MatSliderModule,
+        MatSlideToggleModule
+    ]
+})
+export class MaterialModule { }
