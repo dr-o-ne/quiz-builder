4d8f251890469755692e45fab010719423f93142
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-06-05 23:25:41 +0300

    remove quiz-builder-client folder

--- quiz-builder-client/src/app/_directive/choice.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-  selector: '[appDynamicChoiceHost]',
-})
-export class ChoiceDirective {
-  constructor(public viewContainerRef: ViewContainerRef) { }
-}
