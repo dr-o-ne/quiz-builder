1efe235f2615d3e9f7eb2b6eed3704bf9795c826
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-06-07 15:09:12 +0300

    fix build

--- admin/src/app/question/modal-window/modal-window-preview-question.component.ts
+++ admin/src/app/question/modal-window/modal-window-preview-question.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { QuestionPageComponent } from '../question-page.component';
+import { QuestionPageComponent } from '../question-page/question-page.component';
 import { QuestionType } from 'src/app/_models/question';
 
 @Component({
