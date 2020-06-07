254aa3845c8292252b71de52a7000f20b44f2147
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-06-07 14:37:07 +0300

    move question-page component to separate folder

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { QuizListComponent } from './quiz/quiz-list/quiz-list.component';
 import { QuizPageComponent } from './quiz/quiz-page/quiz-page.component';
 import { QuizAttemptComponent } from './quiz/quiz-attempt/quiz-attempt.component';
 import { QuizResolver } from './_resolvers/quiz.resolver';
-import { QuestionPageComponent } from './question/question-page.component';
+import { QuestionPageComponent } from './question/question-page/question-page.component';
 import { QuestionResolver } from './_resolvers/question.resolver';
 import { PreviewQuizComponent } from './quiz/quiz-preview/preview-quiz.component';
 import { QuizPreviewResolver } from './_resolvers/quizpreview.resolver';
