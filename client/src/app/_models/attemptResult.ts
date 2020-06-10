87558011221ac8e6fca62f21c8dc8382a02308ba
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-06-10 07:03:13 +0300

    add models

---
+++ client/src/app/_models/attemptResult.ts
@@ -0,0 +1,14 @@
+export class QuizAttemptResult {
+    id: string;
+    quizId: string;
+}
+
+export class QuestionAttemptResult {
+    id: string;
+    questionId: string;
+}
+
+export class ChoiceAttemptResult {
+    id: number;
+    text: string;
+}
