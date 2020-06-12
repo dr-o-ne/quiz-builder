389c938ddd1604c574a9837356854229d829a8bd
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-06-12 12:56:29 +0300

    add material design

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizAttemptComponent } from './quiz-attempt/quiz-attempt.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { QuizAttemptComponent } from './quiz-attempt/quiz-attempt.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
