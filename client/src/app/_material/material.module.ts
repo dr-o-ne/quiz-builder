005434a258d09b8bc27e3f4ae18049135f0522eb
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-06-15 23:39:17 +0300

    add submit button

--- client/src/app/_material/material.module.ts
+++ client/src/app/_material/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     exports: [
         MatRadioModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatButtonModule
     ]
 })
 
