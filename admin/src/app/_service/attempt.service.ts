6967a5741645792d59e054b662c53cbaf1841426
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-06-20 00:06:15 +0300

    get rid of unused methods

--- admin/src/app/_service/attempt.service.ts
+++ admin/src/app/_service/attempt.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { InfoChoice } from '../_models/option';
-import { QuizAttempt } from '../_models/attempt';
 
 @Injectable({
   providedIn: 'root'
@@ -28,14 +27,6 @@ export class AttemptService {
 
   resetInfoChoice(): void {
     this.infoChoice.next(null);
-  }
-
-  createAttempt(quizId: string): Observable<object> {
-    return this.http.post(this.apiUrl + 'attempts', { quizId });
-  }
-
-  updateAttempt(quizAttempt: QuizAttempt): Observable<object> {
-    return this.http.put(this.apiUrl + 'attempts', quizAttempt);
   }
 
 }
