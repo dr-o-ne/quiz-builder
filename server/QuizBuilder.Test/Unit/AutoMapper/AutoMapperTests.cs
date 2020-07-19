17b6287a951a68ebf79fc5f718743d39489594cf
Andriy Rebrin <andriy.rebrin@d2l.com>
2020-07-19 17:25:25 +0300

    update automapper version

---
+++ server/QuizBuilder.Test/Unit/AutoMapper/AutoMapperTests.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using QuizBuilder.Domain.Action.Mapper;
+using Xunit;
+
+namespace QuizBuilder.Test.Unit.AutoMapper {
+
+	public sealed class AutoMapperTests {
+
+		[Fact]
+		public void ProfileConfiguration_Test() {
+			var sut = new MapperConfiguration( cfg => cfg.AddProfile<QuizBuilderProfile>() );
+			sut.AssertConfigurationIsValid();
+		}
+	}
+}
