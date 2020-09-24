14b7bac8465ada29d711a0792b19e08e59720595
dr_one <studmm@ukr.net>
2020-09-24 20:30:55 +0300

    cleanup

--- admin-ng/src/app/quiz-builder/_question/choice-info/true-false-choice-info/true-false-choice-info.component.ts
+++ admin-ng/src/app/quiz-builder/_question/choice-info/true-false-choice-info/true-false-choice-info.component.ts
@@ -6,49 +6,49 @@ import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
 import { ChoiceUtilsService } from '../choice-utls.service';
 
 @Component({
-  selector: 'app-true-false-choice-info',
-  templateUrl: './true-false-choice-info.component.html',
-  styleUrls: ['./true-false-choice-info.component.css']
+    selector: 'app-true-false-choice-info',
+    templateUrl: './true-false-choice-info.component.html',
+    styleUrls: ['./true-false-choice-info.component.css']
 })
 
 export class TrueFalseChoiceInfoComponent extends ChoiceBaseDirective implements OnInit {
 
-  constructor(    
-    protected fb: FormBuilder,
-    private choiceUtilsService: ChoiceUtilsService ) {
+    constructor(
+        protected fb: FormBuilder,
+        private choiceUtilsService: ChoiceUtilsService) {
 
-    super(fb);
-  }
+        super(fb);
+    }
 
-  ngOnInit(): void {
-    const choicesForm = this.choiceUtilsService.createBinaryChoicesForm(this.question.choices);
-    this.questionForm.addControl("choices", choicesForm);
-  }
+    ngOnInit(): void {
+        const choicesForm = this.choiceUtilsService.createBinaryChoicesForm(this.question.choices);
+        this.questionForm.addControl("choices", choicesForm);
+    }
 
-  save(): void {
-    // update data
-    this.choiceForm().controls.forEach(
-      (choiceForm: FormGroup) => {
-        const choiceIdForm = choiceForm.get('id').value;
-        const choice = this.question.choices.find(x => x.id == choiceIdForm) as Choice;
+    save(): void {
+        // update data
+        this.choiceForm().controls.forEach(
+            (choiceForm: FormGroup) => {
+                const choiceIdForm = choiceForm.get('id').value;
+                const choice = this.question.choices.find(x => x.id == choiceIdForm) as Choice;
 
-        Object.assign(choice, choiceForm.value);
-      }
-    );
-  }
+                Object.assign(choice, choiceForm.value);
+            }
+        );
+    }
 
-  onChangeChoice(event: MatRadioChange): void {
-    // update form data
-    this.choiceForm().controls.forEach(
-      (x: FormGroup) => {
-        const isSelected = x.get('id').value === event.value
-        x.patchValue({ isCorrect: isSelected });
-      }
-    );
-  }
+    onChangeChoice(event: MatRadioChange): void {
+        // update form data
+        this.choiceForm().controls.forEach(
+            (x: FormGroup) => {
+                const isSelected = x.get('id').value === event.value
+                x.patchValue({ isCorrect: isSelected });
+            }
+        );
+    }
 
-  choiceForm(): FormArray {
-    return this.questionForm.get("choices") as FormArray;
-  }
+    choiceForm(): FormArray {
+        return this.questionForm.get("choices") as FormArray;
+    }
 
 }
