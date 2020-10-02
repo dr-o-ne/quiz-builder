7517fadab8f8ea3bc20b929e6bb112a8421c19c7
dr_one <studmm@ukr.net>
2020-10-02 23:10:21 +0300

    client - tweak UI

--- client-ng/src/app/quiz-builder/_quiz-attempt/questions/true-false-question/true-false-question.component.ts
+++ client-ng/src/app/quiz-builder/_quiz-attempt/questions/true-false-question/true-false-question.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { MatRadioChange } from '@angular/material/radio';
 import { QuestionComponent } from '../question.component';
 import { ChoicesDisplayType } from '../../../model/_enums';
 import { TrueFalseQuestionAttemptInfo } from '../../../model/attemptInfo';
 import { TrueFalseQuestionAttemptResult } from '../../../model/attemptResult';
 import { MatSelectChange } from '@angular/material/select';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
     selector: 'qb-true-false-question',
-    templateUrl: './true-false-question.component.html'
+    templateUrl: './true-false-question.component.html',
+    styleUrls: ['./true-false-question.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
 })
 
 export class TrueFalseQuestionComponent extends QuestionComponent<TrueFalseQuestionAttemptInfo, TrueFalseQuestionAttemptResult> {
