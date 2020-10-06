2bba71aa6c04853d1393541ef8ad6520c87f25fc
dr_one <studmm@ukr.net>
2020-10-06 23:13:41 +0300

    client - submit results

--- client-ng/src/app/quiz-builder/_quiz-attempt/quiz-attempt.component.ts
+++ client-ng/src/app/quiz-builder/_quiz-attempt/quiz-attempt.component.ts
@@ -1,8 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { fuseAnimations } from '@fuse/animations';
+import { QuizAttemptFeedback } from '../model/attemptFeedback';
 import { QuestionAttemptInfo, QuizAttemptInfo } from '../model/attemptInfo';
-import { QuestionAttemptResult } from '../model/attemptResult';
+import { QuestionAttemptResult, QuizAttemptResult } from '../model/attemptResult';
+import { ApiResponse } from '../services/dataProviders/apiResponse';
+import { DataProviderService } from '../services/dataProviders/dataProvider.service';
 
 @Component({
     selector: 'qb-quiz-attempt',
@@ -14,13 +17,16 @@ import { QuestionAttemptResult } from '../model/attemptResult';
 export class QuizAttemptComponent {
 
     attempt!: QuizAttemptInfo;
+    answers!: Map<string, QuestionAttemptResult>;
     currentPageIndex!: number;
 
     constructor(
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private dataProviderService: DataProviderService,
     ) {
         this.attempt = this.route.snapshot.data.attempt;
         this.currentPageIndex = 0;
+        this.answers = new Map<string, QuestionAttemptResult>();
     }
 
     
@@ -44,6 +50,11 @@ export class QuizAttemptComponent {
     }
 
     onAnswer(answer: QuestionAttemptResult): void {
+    }
+
+    submit(): void{
+        const result = new QuizAttemptResult(this.attempt.id, [...this.answers.values()]);
+        this.dataProviderService.endAttempt(result).subscribe((quizAttemptFeedback: ApiResponse<QuizAttemptFeedback>) => {});
     }
 
 }
