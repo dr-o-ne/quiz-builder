f723199ab6d724452b756c9715b99fb2a26e04e9
dr_one <studmm@ukr.net>
2020-10-12 20:57:03 +0300

    client - add Duration card

--- server/QuizBuilder.Domain.Action/Client/ActionHandler/QuizAttemptHandler/GetAttemptFeedbackActionHandler.cs
+++ server/QuizBuilder.Domain.Action/Client/ActionHandler/QuizAttemptHandler/GetAttemptFeedbackActionHandler.cs
@@ -1,22 +1,27 @@
 ﻿using System.Threading.Tasks;
+using AutoMapper;
 using QuizBuilder.Common.CQRS.ActionHandlers;
 using QuizBuilder.Common.CQRS.Actions.Default;
 using QuizBuilder.Data.DataProviders;
 using QuizBuilder.Data.Dto;
 using QuizBuilder.Domain.Action.Client.Action;
 using QuizBuilder.Domain.Action.Client.ActionResult;
+using QuizBuilder.Domain.Model.Default;
 
 namespace QuizBuilder.Domain.Action.Client.ActionHandler.QuizAttemptHandler {
 
 	public sealed class GetAttemptFeedbackActionHandler : IQueryHandler<GetAttemptFeedbackInfoAction, CommandResult<AttemptFeedbackInfo>> {
 
+		private readonly IMapper _mapper;
 		private readonly IQuizAttemptDataProvider _attemptDataProvider;
 		private readonly IQuizDataProvider _quizDataProvider;
 
 		public GetAttemptFeedbackActionHandler(
+			IMapper mapper,
 			IQuizAttemptDataProvider attemptDataProvider,
 			IQuizDataProvider quizDataProvider
 		) {
+			_mapper = mapper;
 			_attemptDataProvider = attemptDataProvider;
 			_quizDataProvider = quizDataProvider;
 		}
@@ -25,14 +30,18 @@ namespace QuizBuilder.Domain.Action.Client.ActionHandler.QuizAttemptHandler {
 
 			AttemptDto attemptDto = await _attemptDataProvider.Get( action.AttemptUId );
 			QuizDto quizDto = await _quizDataProvider.Get( 0, "169", attemptDto.QuizUId );
+			Quiz quiz = _mapper.Map<Quiz>( quizDto );
 
-
-
+			var payload = new AttemptFeedbackInfo();
+			if( quiz.IsResultTotalScoreEnabled )
+				payload.TotalScore = attemptDto.TotalScore;
+			if( quiz.IsResultDurationEnabled )
+				payload.Duration = 100;
 			 
 			return new CommandResult<AttemptFeedbackInfo> {
 				IsSuccess = true,
 				Message = string.Empty,
-				Payload = new AttemptFeedbackInfo {TotalScore = attemptDto.TotalScore}
+				Payload = payload
 			};
 		}
 
